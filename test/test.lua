85086bdd984c0aa4d74ee65e59b1805ba101d0cc
paynechu <314176+paynechu@users.noreply.github.com>
2013-12-12 17:09:44 +0800

    - fixed inner message not point to correct descriptor

--- test/test.lua
+++ test/test.lua
@@ -7,6 +7,7 @@ local person= person_pb.Person()
 person.id = 1000
 person.name = "Alice"
 person.email = "Alice@example.com"
+person.test.field = 'hello'
 
 local home = person.Extensions[person_pb.Phone.phones]:add()
 home.num = "2147483647"
